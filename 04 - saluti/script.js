400c588b38dfcbebf25c27fcf0c29a5bfa6ae9be
martinagiammillari <martigiammi@gmail.com>
2025-11-04 13:52:13 +0100

    feat: saluti done

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -5,10 +5,12 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
-
+function saluta(){
+console.log("ciao " + name)
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+saluta(name)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
