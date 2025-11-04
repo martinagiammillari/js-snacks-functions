d0544c3b8d5d00a727e9e982b00cc20a304519af
martinagiammillari <martigiammi@gmail.com>
2025-11-04 14:06:38 +0100

    feat: iniziali done

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
+
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
 
+function toFirstLetter(names){
+     return names.map(name => name[0]);
+
+}
+
+const firstLetter= toFirstLetter(names);
 
 // Invoca la funzione qui e stampa il risultato in console
 
 
+console.log(firstLetter);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
